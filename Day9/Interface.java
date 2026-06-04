4b921facd07165c28dfa664f396cf8cb7c490a8c
Nandhini1234-lab <nandhini770966@gmail.com>
2026-06-04 20:17:02 +0530

    9th commit

---
+++ Day9/Interface.java
@@ -0,0 +1,23 @@
+
+interface Camera{
+    void clickPhoto();
+}
+interface MusicPlayer{
+    void playMusic();
+}
+class Phone implements Camera,MusicPlayer{
+     public void clickPhoto(){
+        System.out.println("Photo taken from mobile");
+    }
+    public void  playMusic(){
+        System.out.println("Music playing in phone");
+    }
+}
+public class Interface{
+    public static void main(String[]args)
+    {
+         Phone p=new Phone();
+        p.clickPhoto();
+        p.playMusic();
+    }
+}
